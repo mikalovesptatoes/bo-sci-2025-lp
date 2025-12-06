636bd07d26870fecea1d778117ad01a11a6df3ae
mika <ka11550070@keio.jp>
2025-12-07 01:41:19 +0900

    add test heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<h1 style={{ fontSize: '40px', color: 'red' }}>TEST UPDATE 999</h1>
 import { Hero } from './components/Hero';
 import { ImageSlideshow } from './components/ImageSlideshow';
 import { ReviewsCarousel } from './components/ReviewsCarousel';
@@ -10,17 +9,22 @@ import { Footer } from './components/Footer';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Hero />
-      <ImageSlideshow />
-      <ReviewsCarousel />
-      <Features />
-      <DemoExperience />
-      <WorkshopDetails />
-      <Footer />
-      <FixedApplyButton />
-      {/* 固定ボタンのスペース確保 */}
-      <div className="h-20"></div>
-    </div>
+    <>
+      {/* テスト用の大きな赤文字 */}
+      <h1 style={{ fontSize: '40px', color: 'red' }}>TEST UPDATE 999</h1>
+
+      <div className="min-h-screen bg-white">
+        <Hero />
+        <ImageSlideshow />
+        <ReviewsCarousel />
+        <Features />
+        <DemoExperience />
+        <WorkshopDetails />
+        <Footer />
+        <FixedApplyButton />
+        {/* 固定ボタンのスペース確保 */}
+        <div className="h-20"></div>
+      </div>
+    </>
   );
 }
